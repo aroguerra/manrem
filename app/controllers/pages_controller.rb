c207c04603a05224ad085dd71ba5a9eb1ee5bec1
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-05-22 19:40:23 +0100

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,15 +7,13 @@ class PagesController < ApplicationController
   def dashboard
     @my_simulations = Simulation.where(user_id: current_user.id)
     @my_simulations_pool = Simulation.where(user_id: current_user.id, market_type: "pool market")
-    @my_simulations_pool_asym = Simulation.where(user_id: current_user.id, market_type: "pool market")
-
+    @my_simulations_pool_asym = Simulation.where(user_id: current_user.id, market_type: "pool market", pricing_mechanism: "assymetrical")
+    @my_simulations_pool_sym = Simulation.where(user_id: current_user.id, market_type: "pool market", pricing_mechanism: "symetrical")
 
 
     @my_agents = Agent.where(user_id: current_user.id)
     @my_buyers = Agent.where(user_id: current_user.id, category: "Buyer")
     @my_sellers = Agent.where(user_id: current_user.id, category: "Seller")
-
-
   end
 
- end
+end
