0b481919a90b6e85572c2fa2d30518005f829f3c
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-05-26 00:13:16 +0100

    a

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -2,5 +2,7 @@ class SimulationsController < ApplicationController
 
   def index
     @simulations = Simulation.where(user_id: current_user.id).order('date DESC')
+    @my_buyers = Agent.where(user_id: current_user.id, category: "Buyer")
+    @my_sellers = Agent.where(user_id: current_user.id, category: "Seller")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: "pages#dashboard"
+
   get  "simulation", to: "simulations#index"
+  get "simulation/:simulation_id/results", to: "results#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
