96dc107f1bcd79ae243eaac28ff21a885c14a70b
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-05-25 18:57:20 +0100

    simulations list

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -1,6 +1,6 @@
 class SimulationsController < ApplicationController
 
   def index
-    @simulations = Simulation.where(user_id: current_user.id)
+    @simulations = Simulation.where(user_id: current_user.id).order('date DESC')
   end
 end
