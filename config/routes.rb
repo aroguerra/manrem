014ec35bd5615097155a1370c3f1df2f03deede0
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-05-29 13:19:39 +0100

    agents page created, some css on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   get  "simulation", to: "simulations#index"
   get "simulation/:simulation_id/results", to: "results#show", as: 'results_show'
+  get  "agents", to: "agents#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
