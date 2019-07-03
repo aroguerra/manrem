2c16daa0e3101af66fb3c3e37577815d6700c2f8
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-07-03 02:20:49 +0100

    sym asym almost

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -9,216 +9,232 @@ class SimulationsController < ApplicationController
     @my_buyers = Agent.where(user_id: current_user.id, category: "Buyer")
     @my_sellers = Agent.where(user_id: current_user.id, category: "Seller")
 
-    bids = []
-    offers = []
-    offer_power = 0
-    last_seller_price = 0
-    lower_limit = 0
-    pos = 0
-    i = 0
-    flag_price = false
-    flag_end = false
-    period_market_price = 0
+    # simulation_sym = Simulation.new(
+    #   date: DateTime.now,
+    #   market_type: "pool market",
+    #   pricing_mechanism: "symetrical",
+    #   user_id: current_user.id)
 
-    ########### get offers and bids by order of price in each period#########
-    @my_buyers.each do |buyer|
-      bids << buyer.offers.where(period: 1)
-    end
-    buyers_bids = bids.flatten.sort_by { |bid| bid.price }
+    (1..24).each do |per|
+      byebug
+      bids = []
+      offers = []
+      offer_power = 0
+      last_seller_price = 0
+      lower_limit = 0
+      pos = 0
+      i = 0
+      flag_price = false
+      flag_end = false
+      period_market_price = 0
 
-    @my_sellers.each do |seller|
-      offers << seller.offers.where(period: 1)
-    end
-    sellers_offers = offers.flatten.sort_by { |offer| offer.price }
-
-    demand_power = buyers_bids.sum { |a| a.energy }
-    upper_limit = buyers_bids[0].price
-    last_seller_tosold_price = sellers_offers[0].price
-
-    # ##############Set market price##############
-    while flag_price == false && flag_end == false
-      sellers_offers.each do |offer|
-        offer_power += offer.energy
-        last_seller_tosold_price = offer.price
-        i += 1
-        break if offer_power >= demand_power
+      ########### get offers and bids by order of price in each period#########
+      @my_buyers.each do |buyer|
+        bids << buyer.offers.where(period: per)
       end
+      buyers_bids = bids.flatten.sort_by { |bid| bid.price }
 
-      if sellers_offers.size > i + 1
-        last_seller_price = sellers_offers[i + 1].price
-      else
-        last_seller_price = last_seller_tosold_price
+      @my_sellers.each do |seller|
+        offers << seller.offers.where(period: per)
       end
+      sellers_offers = offers.flatten.sort_by { |offer| offer.price }
 
-      if last_seller_tosold_price <= upper_limit
-        if last_seller_tosold_price >= lower_limit
-          period_market_price = last_seller_tosold_price
-        elsif lower_limit > last_seller_tosold_price
-          if lower_limit < last_seller_price
-            period_market_price = lower_limit
-          else
+      demand_power = buyers_bids.sum { |a| a.energy }
+      upper_limit = buyers_bids[0].price
+      last_seller_tosold_price = sellers_offers[0].price
+
+      # ##############Set market price##############
+      while flag_price == false && flag_end == false
+        sellers_offers.each do |offer|
+          offer_power += offer.energy
+          last_seller_tosold_price = offer.price
+          i += 1
+          break if offer_power >= demand_power
+        end
+
+        if sellers_offers.size > i + 1
+          last_seller_price = sellers_offers[i + 1].price
+        else
+          last_seller_price = last_seller_tosold_price
+        end
+
+        if last_seller_tosold_price <= upper_limit
+          if last_seller_tosold_price >= lower_limit
             period_market_price = last_seller_tosold_price
+          elsif lower_limit > last_seller_tosold_price
+            if lower_limit < last_seller_price
+              period_market_price = lower_limit
+            else
+              period_market_price = last_seller_tosold_price
+            end
           end
+          flag_price = true
         end
-        flag_price = true
-      end
-      if pos < buyers_bids.size - 1
-        demand_power -= buyers_bids[pos].energy
-        lower_limit = upper_limit
-        pos += pos
-        upper_limit = buyers_bids[pos].price
-      else
-        flag_end = true
-      end
-    end
-
-    #### Set offers accepted#####
-    demand_accepted = 0
-    buyers_bids.each do |bid|
-      demand_accepted += bid.energy if bid.price >= period_market_price
-    end
-
-    offer_accepted = 0
-    sellers_offers.each do |offer|
-      offer_accepted += offer.energy if offer.price <= period_market_price
-    end
-
-    #### Set total power accepted#####
-    total_power_sold = 0
-    demand_accepted > offer_accepted ? total_power_sold = offer_accepted : total_power_sold = demand_accepted
-
-    #### inform buyers if bids were accepted or not
-    power = total_power_sold
-    buyers_bids.reverse!
-
-    buyers_traded_power = []
-    sellers_traded_power = []
-
-    buyers_bids.each do |bid|
-      if bid.price >= period_market_price
-        if power > bid.energy
-          ####criar resultado com traded_power = bid.power####
-          buyers_traded_power << bid.energy
-          power -= bid.energy
+        if pos < buyers_bids.size - 1
+          demand_power -= buyers_bids[pos].energy
+          lower_limit = upper_limit
+          pos += pos
+          upper_limit = buyers_bids[pos].price
         else
-          #### criar resultado com traded_power = power
-          buyers_traded_power << power
-          power = 0
+          flag_end = true
         end
-      else
-        #### criar resultado com traded_power = 0 (no buy_bolsa)
-        buyers_traded_power << 0
       end
-    end
 
-    #### inform sellers if offers were accepted or not
-    power = total_power_sold
-    sellers_offers.each do |offer|
-      if offer.price <= period_market_price
-        if power > offer.energy
-          ####criar resultado com traded_power = bid.power####
-          sellers_traded_power << offer.energy
-          power -= offer.energy
+      #### Set offers accepted#####
+      demand_accepted = 0
+      buyers_bids.each do |bid|
+        demand_accepted += bid.energy if bid.price >= period_market_price
+      end
+
+      offer_accepted = 0
+      sellers_offers.each do |offer|
+        offer_accepted += offer.energy if offer.price <= period_market_price
+      end
+
+      #### Set total power accepted#####
+      total_power_sold = 0
+      demand_accepted > offer_accepted ? total_power_sold = offer_accepted : total_power_sold = demand_accepted
+
+      #### inform buyers if bids were accepted or not
+      power = total_power_sold
+      buyers_bids.reverse!
+
+      buyers_traded_power = []
+      sellers_traded_power = []
+
+      buyers_bids.each do |bid|
+        if bid.price >= period_market_price
+          if power > bid.energy
+            ####criar resultado com traded_power = bid.power####
+            buyers_traded_power << bid.energy
+            power -= bid.energy
+          else
+            #### criar resultado com traded_power = power
+            buyers_traded_power << power
+            power = 0
+          end
         else
-          #### criar resultado com traded_power = power
-          sellers_traded_power << power
-          power = 0
+          #### criar resultado com traded_power = 0 (no buy_bolsa)
+          buyers_traded_power << 0
         end
-      else
-        #### criar resultado com traded_power = 0 (no buy_bolsa)
-        sellers_traded_power << 0
+      end
+
+      #### inform sellers if offers were accepted or not
+      power = total_power_sold
+      sellers_offers.each do |offer|
+        if offer.price <= period_market_price
+          if power > offer.energy
+            ####criar resultado com traded_power = bid.power####
+            sellers_traded_power << offer.energy
+            power -= offer.energy
+          else
+            #### criar resultado com traded_power = power
+            sellers_traded_power << power
+            power = 0
+          end
+        else
+          #### criar resultado com traded_power = 0 (no buy_bolsa)
+          sellers_traded_power << 0
+        end
       end
     end
+    redirect_to simulation_path
   end
 
   def asym
     @my_buyers = Agent.where(user_id: current_user.id, category: "Buyer")
     @my_sellers = Agent.where(user_id: current_user.id, category: "Seller")
 
-    bids = []
-    offers = []
-    period_market_price = 0
-    ########### get offers and bids by order of price in each period#########
-    @my_buyers.each do |buyer|
-      bids << buyer.offers.where(period: 1)
-    end
-    buyers_bids = bids.flatten
+    # simulation_asym = Simulation.new(
+    #   date: DateTime.now,
+    #   market_type: "pool market",
+    #   pricing_mechanism: "assymetrical",
+    #   user_id: current_user.id)
 
-    @my_sellers.each do |seller|
-      offers << seller.offers.where(period: 1)
-    end
-    sellers_offers = offers.flatten.sort_by { |offer| offer.price }
+    (1..24).each do |per|
+      bids = []
+      offers = []
+      period_market_price = 0
+      ########### get offers and bids by order of price in each period#########
+      @my_buyers.each do |buyer|
+        bids << buyer.offers.where(period: per)
+      ends
+      buyers_bids = bids.flatten
 
-    demand_power = buyers_bids.sum { |a| a.energy }
+      @my_sellers.each do |seller|
+        offers << seller.offers.where(period: per)
+      end
+      sellers_offers = offers.flatten.sort_by { |offer| offer.price }
 
-    # ##############Set market price##############
+      demand_power = buyers_bids.sum { |a| a.energy }
 
-    accepted_power = 0
+      # ##############Set market price##############
 
-    if demand_power > 0
+      accepted_power = 0
+
+      if demand_power > 0
+        sellers_offers.each do |offer|
+          accepted_power < demand_power ? accepted_power += offer.energy && period_market_price = offer.price : break
+        end
+      end
+
+      #### Set offers accepted#####
+      demand_accepted = 0
+      demand_accepted = demand_power
+      demand_accepted = accepted_power if demand_power > accepted_power
+
+      offer_accepted = 0
       sellers_offers.each do |offer|
-        accepted_power < demand_power ? accepted_power += offer.energy && period_market_price = offer.price : break
+        offer_accepted += offer.energy if offer.price <= period_market_price
       end
-    end
 
-    #### Set offers accepted#####
-    demand_accepted = 0
-    demand_accepted = demand_power
-    demand_accepted = accepted_power if demand_power > accepted_power
-
-    offer_accepted = 0
-    sellers_offers.each do |offer|
-      offer_accepted += offer.energy if offer.price <= period_market_price
-    end
-
-    #### Set total power accepted#####
-    total_power_sold = 0
-    demand_accepted > offer_accepted ? total_power_sold = offer_accepted : total_power_sold = demand_accepted
+      #### Set total power accepted#####
+      total_power_sold = 0
+      demand_accepted > offer_accepted ? total_power_sold = offer_accepted : total_power_sold = demand_accepted
 
 
-    #### inform buyers if bids were accepted or not
-    power = total_power_sold
+      #### inform buyers if bids were accepted or not
+      power = total_power_sold
 
-    buyers_traded_power = []
-    sellers_traded_power = []
+      buyers_traded_power = []
+      sellers_traded_power = []
 
-    buyers_bids.each do |bid|
-      if power > 0
-        if power > bid.energy
-          ####criar resultado com traded_power = bid.power####
-          buyers_traded_power << bid.energy
-          power -= bid.energy
+      buyers_bids.each do |bid|
+        if power > 0
+          if power > bid.energy
+            ####criar resultado com traded_power = bid.power####
+            buyers_traded_power << bid.energy
+            power -= bid.energy
+          else
+            #### criar resultado com traded_power = power
+            buyers_traded_power << power
+            power = 0
+          end
         else
-          #### criar resultado com traded_power = power
-          buyers_traded_power << power
-          power = 0
+          #### criar resultado com traded_power = 0 (no buy_bolsa)
+          buyers_traded_power << 0
         end
-      else
-        #### criar resultado com traded_power = 0 (no buy_bolsa)
-        buyers_traded_power << 0
       end
-    end
 
-    #### inform sellers if offers were accepted or not
-    power = total_power_sold
-    sellers_offers.each do |offer|
-      if offer.price <= period_market_price
-        if power > offer.energy
-          ####criar resultado com traded_power = bid.power####
-          sellers_traded_power << offer.energy
-          power -= offer.energy
+      #### inform sellers if offers were accepted or not
+      power = total_power_sold
+      sellers_offers.each do |offer|
+        if offer.price <= period_market_price
+          if power > offer.energy
+            ####criar resultado com traded_power = bid.power####
+            sellers_traded_power << offer.energy
+            power -= offer.energy
+          else
+            #### criar resultado com traded_power = power
+            sellers_traded_power << power
+            power = 0
+          end
         else
-          #### criar resultado com traded_power = power
-          sellers_traded_power << power
-          power = 0
+          #### criar resultado com traded_power = 0 (no buy_bolsa)
+          sellers_traded_power << 0
         end
-      else
-        #### criar resultado com traded_power = 0 (no buy_bolsa)
-        sellers_traded_power << 0
       end
     end
-
-
-
+    redirect_to simulation_path
   end
 end
