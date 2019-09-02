6368d999f9db48d7a36b83bf6bb8b771ac870375
abraham guerra <ar.guerra@campus.fct.unl.pt>
2019-09-02 12:55:49 +0100

    excel ter

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -32,16 +32,19 @@ class ResultsController < ApplicationController
   def showbmter
     @simulation = Simulation.find(params[:simulation_id])
     @results = BmTerciaryResult.where(simulation_id: Simulation.find(params[:simulation_id]))
+    @a = @results.where(simulation_id: @simulation.id).sort_by {|result| [result.bm_unit_name, result.period]}
+
     # @results.each do |result|
-    #   result.ter_need_up.positive? ? @up << result : @down << result
+    #   result.ter_need_up.zero? ? @down << result : @up << result
     # end
-    # respond_to do |format|
-    #   format.xlsx {
-    #     response.headers[
-    #       'Content-Disposition'
-    #     ] = "attachment; filename=items.xlsx"
-    #   }
-    #   format.html { render :showbm }
-    # end
+
+    respond_to do |format|
+      format.xlsx {
+        response.headers[
+          'Content-Disposition'
+        ] = "attachment; filename=items.xlsx"
+      }
+      format.html { render :showbmter }
+    end
   end
 end
